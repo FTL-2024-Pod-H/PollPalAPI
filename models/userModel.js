9edf9969819ec51ed7a8732017082baf165d1509
Hanna Abrahem <habrahem@haverford.edu>
2024-07-18 22:28:40 -0700

    Usermodel created

---
+++ models/userModel.js
@@ -0,0 +1,29 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+// function to Register user -- (create prisma)
+const createUser = async (username, password) => {
+    return await prisma.user.create({
+        data: {username, password}
+    })
+}
+
+// function to Login in user (findUnique prisma)
+// find user by id/username
+const findUserByUsername = async (username) => {
+    return await prisma.user.findUnique({
+        where: {username}
+    })
+}
+
+const findUserById = async (id) => {
+    return await prisma.user.findUnique({
+        where: {id },
+    });
+};
+
+module.exports = {
+    createUser,
+    findUserByUsername,
+    findUserById
+}
