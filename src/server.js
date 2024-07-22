e20c04121cc0ca483faed453f05b1f4683d47138
Kiahna Isadore <kiahnaisadore@gmail.com>
2024-07-22 09:54:56 -0700

    update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express()
 const cors = require("cors");
 const morgan = require("morgan");
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 //importing userRoutes
 const userRoutes = require("../routes/userRoutes");
