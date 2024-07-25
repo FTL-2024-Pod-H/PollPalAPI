128b8d3eab402db2d8c433b38b017a9e7b8aa101
Hanna Abrahem <habrahem@haverford.edu>
2024-07-25 10:41:49 -0700

    added google_id

--- models/userModel.js
+++ models/userModel.js
@@ -2,28 +2,29 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 // function to Register user -- (create prisma)
-const createUser = async (name, username, password, address) => { //maybe need to add name, address
-    return await prisma.user.create({
-        data: {name, address, username, password} //maybe need to add name, address
-    })
-}
+const createUser = async (name, username, password, address, googleId = "0") => {
+  //maybe need to add name, address
+  return await prisma.user.create({
+    data: { name, address, username, password, googleId },
+  });
+};
 
 // function to Login in user (findUnique prisma)
 // find user by id/username
 const findUserByUsername = async (username) => {
-    return await prisma.user.findUnique({
-        where: { username }
-    })
-}
+  return await prisma.user.findUnique({
+    where: { username },
+  });
+};
 
 const findUserById = async (id) => {
-    return await prisma.user.findUnique({
-        where: {id },
-    });
+  return await prisma.user.findUnique({
+    where: { id },
+  });
 };
 
 module.exports = {
-    createUser,
-    findUserByUsername,
-    findUserById
-}
+  createUser,
+  findUserByUsername,
+  findUserById,
+};
